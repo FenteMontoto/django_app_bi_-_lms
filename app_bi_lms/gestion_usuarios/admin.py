57523f3e40c91165b81d5f32953b0bfd46892c3a
FenteMontoto <fentemontoto@gmail.com>
2022-11-13 11:39:15 +0100

    Configuración vistas del panel de control

--- app_bi_lms/gestion_usuarios/admin.py
+++ app_bi_lms/gestion_usuarios/admin.py
@@ -3,7 +3,20 @@ from gestion_usuarios.models import Empleado
 from gestion_usuarios.models import Curso
 from gestion_usuarios.models import Formacion
 # Register your models here.
-
-admin.site.register(Empleado)
-admin.site.register(Curso)
-admin.site.register(Formacion)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display=('cod_empleado','nombre','primer','segundo','departamento')
+    search_fields=('cod_empleado','nombre','primer','segundo','departamento')
+    list_filter=('departamento','discapacidad','reduccion_jornada',)
+    
+class CursoAdmin(admin.ModelAdmin):
+    list_display=('cod_curso','nombre_curso')
+    search_fields=('cod_curso','nombre_curso')
+   
+class FormacionAdmin(admin.ModelAdmin):
+    list_display=('gestor','curso','fecha_inicio_curso','fecha_finalizacion_curso','estado_curso')
+    search_fields=('gestor','curso','fecha_inicio_curso','fecha_finalizacion_curso','estado_curso')
+    list_filter=('estado_curso','fecha_finalizacion_curso',)
+    
+admin.site.register(Empleado, EmpleadoAdmin)
+admin.site.register(Curso,CursoAdmin)
+admin.site.register(Formacion,FormacionAdmin)
--- app_bi_lms/gestion_usuarios/models.py
+++ app_bi_lms/gestion_usuarios/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Empleado(models.Model):
+    
     cod_empleado=models.CharField(max_length=6, primary_key=True, unique=True)
     nombre=models.CharField(max_length=30)
-    primer=models.CharField(max_length=30)
-    segundo=models.CharField(max_length=30)
+    primer=models.CharField(max_length=30, verbose_name="Primer apellido")
+    segundo=models.CharField(max_length=30, verbose_name="Segundo apellido")
     email=models.EmailField(blank=True, null=True)
     extension=models.IntegerField()
-    delegacion=models.CharField(max_length=30)
-    grupo_titular=models.CharField(max_length=30)
-    servicio_titular=models.CharField(max_length=30)
+    departamento=models.CharField(max_length=30,blank=True, null=True)
+    delegacion=models.CharField(max_length=30,blank=True, null=True)
+    grupo_titular=models.CharField(max_length=30,blank=True, null=True)
+    servicio_titular=models.CharField(max_length=30,blank=True, null=True)
     grupo_refuerzo=models.CharField(max_length=30,blank=True, null=True)
     servicio_refuerzo_1=models.CharField(max_length=30,blank=True, null=True)
     servicio_refuerzo_2=models.CharField(max_length=30,blank=True, null=True)
@@ -20,12 +22,17 @@ class Empleado(models.Model):
     discapacidad=models.CharField(max_length=2)
     reduccion_jornada=models.CharField(max_length=2)
     horas_jornada=models.IntegerField()
+    
+    def __str__(self):
+        return self.cod_empleado
  
 
 class Curso(models.Model):
-    cod_curso=models.CharField(max_length=6)
-    nombre_curso=models.CharField(max_length=70, primary_key="True", unique="True")
-    tipo_curso=models.CharField(max_length=20)
+    cod_curso=models.CharField(max_length=6,verbose_name="Código curso")
+    nombre_curso=models.CharField(max_length=70,verbose_name="Nombre del curso", primary_key="True", unique="True")
+    tipo_curso=models.CharField(max_length=20,verbose_name="Tipo de curso")
+    def __str__(self):
+        return self.nombre_curso
     
 class Formacion(models.Model):
     gestor=models.ForeignKey(Empleado,on_delete=models.CASCADE,)
@@ -33,4 +40,4 @@ class Formacion(models.Model):
     fecha_matricula_curso=models.DateField()
     fecha_inicio_curso=models.DateField()
     fecha_finalizacion_curso=models.DateField()
-    estado_curso=models.CharField(max_length=20)
+    estado_curso=models.CharField(max_length=20,verbose_name="Estado")
