7d0d8a5de100c8a191622a65aaa04c5ff6c2ad45
FenteMontoto <fentemontoto@gmail.com>
2022-11-14 18:41:09 +0100

    Primer paso de creacion de la plantilla base.html

--- app_bi_lms/gestion_usuarios/views.py
+++ app_bi_lms/gestion_usuarios/views.py
@@ -4,20 +4,21 @@ from django.shortcuts import render, HttpResponse
 
 def login(request):
     
-    return render(request,'gestion_usuarios/login.html')
+    return render(request,'gestion_usuarios/login.html',({"title":"Home"}))
 
 def home(request):
+    
 
-    return render(request,'gestion_usuarios/home.html')
+    return render(request,'gestion_usuarios/home.html',({"title":"Home"}))
 
 def formacion(request):
     
-    return render(request,'gestion_usuarios/formacion.html')
+    return render(request,'gestion_usuarios/formacion.html',({"title":"Formación"}))
 
 def mensajeria(request):
     
-    return render(request,'gestion_usuarios/mensajeria.html')
+    return render(request,'gestion_usuarios/mensajeria.html',({"title":"Mensajería"}))
 
 def mi_actividad(request):
     
-    return render(request,'gestion_usuarios/mi_actividad.html')
+    return render(request,'gestion_usuarios/mi_actividad.html',({"title":"Mi actividad"}))
