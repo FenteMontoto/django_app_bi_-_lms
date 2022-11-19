a9f35abd46f97cf6b66f3682fb2dac9de1f206a8
FenteMontoto <fentemontoto@gmail.com>
2022-11-19 14:10:28 +0100

    cambio de estructura layout

--- app_bi_lms/gestion_usuarios/static/gestion_usuarios/js/funciones.js
+++ app_bi_lms/gestion_usuarios/static/gestion_usuarios/js/funciones.js
@@ -1,19 +1,7 @@
-let flecha_izq = document.getElementById("flecha_izquierda")
-let flecha_der = document.getElementById("flecha_derecha")
-let sidebar = document.getElementById("sidebar")
-let linkitos = document.getElementById("linkitos_ul")
-
-
-flecha_izq.addEventListener('click', () => {
-    flecha_izq.style.visibility = 'hidden'
-    flecha_der.style.visibility = 'visible'
-    sidebar.style.width = "10vh"
-    linkitos.style.display = "none"
-});
-
-flecha_der.addEventListener('click', () => {
-    flecha_der.style.visibility = 'hidden'
-    flecha_izq.style.visibility = 'visible'
-    sidebar.style.width = "25%"
-    linkitos.style.display = ""
+let show_menu = document.getElementById("show_menu")
+let nav = document.getElementById('nav')
+show_menu.addEventListener('click', () => {
+    if (nav.style.display == "none") {
+        nav.style.display = "block"
+    } else { nav.style.display = "none" }
 });
